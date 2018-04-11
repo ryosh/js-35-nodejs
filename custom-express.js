9860bcdf5871a8e8824be79605b05e96c308dd69
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2018-04-11 18:51:32 -0300

    ex2.4

--- custom-express.js
+++ custom-express.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 module.exports = function () {
     const app = express();
+
+    app.use(express.static('./public'));
+    
     app.set('view engine', 'ejs');
 
     require('./routes/produtos')(app);
--- routes/produtos.js
+++ routes/produtos.js
@@ -1,5 +1,21 @@
 module.exports = function (app) {
     app.get('/produtos', function (req, res) {
-        res.render('produtos/lista');
+        const mysql = require('mysql');
+
+        const connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'casadocodigo'
+        });
+
+        connection.query('SELECT * FROM livros',
+            function (err, result, fields) {
+                res.render('produtos/lista', {
+                    lista: result
+                });
+            }
+        );
+        connection.end();
     });
 };
