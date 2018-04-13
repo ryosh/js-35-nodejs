c2c1798c49f1bb6cacab8e2de5d54c6697b0020a
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2018-04-13 16:30:50 -0300

    ex7.6

--- infra/connectionFactory.js
+++ infra/connectionFactory.js
@@ -1,11 +1,16 @@
 var mysql = require('mysql');
 
+let databaseName = 'casadocodigo';
+if (process.env.NODE_ENV == 'test') {
+    databaseName = 'casadocodigo_teste';
+}
+
 function createDBConnection() {
     return mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: '',
-        database: 'casadocodigo'
+        database: databaseName
     });
 }
 
