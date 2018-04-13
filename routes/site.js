ca281e830ca5e48db367019d91b6bf32de635a8d
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2018-04-13 19:11:29 -0300

    ex8.2

--- routes/site.js
+++ routes/site.js
@@ -1,7 +1,7 @@
 module.exports = function (app) {
-    app.get("/", function (req, res) {
-        var connection = app.infra.connectionFactory();
-        var produtos = new app.infra.ProdutoDao(connection);
+    app.get('/', function (req, res) {
+        const connection = app.infra.connectionFactory();
+        const produtos = new app.infra.ProdutoDao(connection);
         produtos.lista(function (error, results, fields) {
             res.render('home/index', {
                 livros: results
